187f57bfe056bf61703a9025fbb35042eaee2e9e
nateasseraf <nateasseraf@gmail.com>
2025-10-07 20:53:00 -0400

    is trying to figure out if he did this right

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+// external javascript 
+let sum=a+b;
+
+let name="Nate";
+let eligibility=false; //BOOLEAN
+let dob="5 April 1999";
+let firstName="Nathaniel"; //camelCasing
+let lastName="Asseraf";
+
+// Way to display message for testing 
+console.log("My full name is "+firstName+lastName);
+const PI=3.1415
+
+let radius = 2;
+
+let area = PI * (radius**2);
